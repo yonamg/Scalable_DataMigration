3965db122d18d2f363ba5c06237481b7f94405ff
yonamg <softeng2006@gmail.com>
2022-09-23 23:37:46 +0300

    initilaize airflow

--- airflow/webserver_config.py
+++
@@ -1,9 +0,0 @@
-"""Default configuration for the Airflow webserver"""
-import os
-
-from airflow.www.fab_security.manager import 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = True
-AUTH_TYPE = AUTH_DB
